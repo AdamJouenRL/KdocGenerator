5dc4e6ef2d2b4d7426d1a3b65e5528351a745725
GitHub Action <action@github.com>
2021-12-28 15:23:41 +0000

    Template cleanup

--- src/main/kotlin/com/github/adamjouenrl/kdocgenerator/MyBundle.kt
+++ src/main/kotlin/com/github/adamjouenrl/kdocgenerator/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.adamjouenrl.kdocgenerator
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/adamjouenrl/kdocgenerator/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/adamjouenrl/kdocgenerator/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.adamjouenrl.kdocgenerator.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.adamjouenrl.kdocgenerator.MyBundle
 
 class MyApplicationService {
 
